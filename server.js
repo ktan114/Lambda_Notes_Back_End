94355973e6c87a3218c91c30bada211eed9e9603
ktan114 <ktaneight@gmail.com>
2018-10-23 18:39:04 -0400

    Changed port number

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ server.get('/', (req, res) => {
 })
 
 // Connect to port
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server running on port: ${port}`))
