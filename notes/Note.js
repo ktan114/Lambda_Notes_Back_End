922c5b6741bd27a077051601d840eabfcecb507b
ktan114 <ktaneight@gmail.com>
2018-10-23 18:58:10 -0400

    Run prettier and added line of code to prevent pre save hook from running if password is not changed

--- notes/Note.js
+++ notes/Note.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const noteSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    body: String,
-    createdBy: {
-        type: ObjectId,
-        ref: 'User',
-        required: true
-    }
-})
+  title: {
+    type: String,
+    required: true
+  },
+  body: String,
+  createdBy: {
+    type: ObjectId,
+    ref: "User",
+    required: true
+  }
+});
 
-module.exports = mongoose.model('Note', noteSchema, 'notes');
+module.exports = mongoose.model("Note", noteSchema, "notes");
--- users/User.js
+++ users/User.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-    }
-})
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
 
-userSchema.pre('save', function(next){
-    return bcrypt.hash(this.password, 11, (err, hash) => {
-        if (err) {
-            return next(err)
-        } 
-        this.password = hash;
-        
-        return next()
-    })
-})
+userSchema.pre("save", function(next) {
+  if (!this.isModified("password")) return next();
+  bcrypt.hash(this.password, 11, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = hash;
+    next();
+  });
+});
 
 userSchema.methods.validatePassword = function(guess, cb) {
-    return bcrypt.compare(guess, this.password)
-}
+  return bcrypt.compare(guess, this.password);
+};
 
-module.exports = mongoose.model('User', userSchema, 'users');
+module.exports = mongoose.model("User", userSchema, "users");
