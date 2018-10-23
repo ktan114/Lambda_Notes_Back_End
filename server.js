88be31546622411ae4301563a8933d49b6e9264d
ktan114 <ktaneight@gmail.com>
2018-10-23 18:54:02 -0400

    Edit server code to include helmet, and used prettier on the code

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const cors = require("cors");
+require("dotenv").config();
 
-// Routes
-const noteRouter = require('./notes/noteRoutes');
-const userRouter = require('./users/userRoutes');
+const noteRouter = require("./notes/noteRoutes");
+const userRouter = require("./users/userRoutes");
 
 // Initialize Server
 const server = express();
 
 // Connect to mlab
-const mongoDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds139920.mlab.com:39920/lambdanotes`;
+const mongoDB = `mongodb://${process.env.DB_USER}:${
+  process.env.DB_PASSWORD
+}@ds139920.mlab.com:39920/lambdanotes`;
 mongoose
-    .connect(mongoDB)
-    .then(connect => {
-        console.log('Connected!');
-    })
-    .catch(err => {
-        console.log('Not connected');
-})
+  .connect(mongoDB)
+  .then(connect => {
+    console.log("Connected!");
+  })
+  .catch(err => {
+    console.log("Not connected");
+  });
 
 // Middleware
 server.use(express.json());
 server.use(cors());
-server.use('/api', noteRouter);
-server.use('/api', userRouter);
+server.use(helmet());
 
-// Initial GET
-server.get('/', (req, res) => {
-    res.send({ Message: 'api running' })
-})
+// Routes
+server.use("/notes", noteRouter);
+server.use("/users", userRouter);
+
+// Initial GET - Remove during production
+server.get("/", (req, res) => {
+  res.send({ Message: "api running" });
+});
 
 // Connect to port
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`Server running on port: ${port}`))
+server.listen(port, () => console.log(`Server running on port: ${port}`));
