47cb77032c9eb6bd8210339e910a4597f9fc74de
ktan114 <ktaneight@gmail.com>
2018-10-26 23:35:19 -0400

    Removed unnecessary route

--- server.js
+++ server.js
@@ -32,11 +32,6 @@ server.use(helmet());
 server.use("/notes", noteRouter);
 server.use("/users", userRouter);
 
-// Initial GET - Remove during production
-server.get("/", (req, res) => {
-  res.send({ Message: "api running" });
-});
-
 // Connect to port
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server running on port: ${port}`));
