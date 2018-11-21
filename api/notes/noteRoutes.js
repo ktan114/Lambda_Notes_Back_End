01bd4caf2d8fc96856f6c9f7bf91efc1eb5978da
ktan114 <ktaneight@gmail.com>
2018-11-20 19:35:50 -0500

    Protect note routes

--- api/notes/noteRoutes.js
+++ api/notes/noteRoutes.js
@@ -29,68 +29,84 @@ router.get(
     @desc   Create a new note
     @access Private (Production) | Public (Development)
 */
-router.post("/", (req, res) => {
-  Note.create(req.body)
-    .then(() => {
-      res.status(201);
-    })
-    .catch(() => {
-      res.status(400).json({ Err: "error creating note" });
-    });
-});
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Note.create(req.body)
+      .then(() => {
+        res.status(201);
+      })
+      .catch(() => {
+        res.status(400).json({ Err: "error creating note" });
+      });
+  }
+);
 
 /*
     @route  GET notes/:id
     @desc   Retrieve a specific note using ID
     @access Private (Production) | Public (Development)
 */
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const id = req.params.id;
 
-  Note.findById(id)
-    .select("title body id createdBy")
-    .populate("createdBy", "username -_id")
-    .then(note => {
-      res.status(200).json(note);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+    Note.findById(id)
+      .select("title body id createdBy")
+      .populate("createdBy", "username -_id")
+      .then(note => {
+        res.status(200).json(note);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  }
+);
 
 /*
     @route  PUT notes/:id
     @desc   Edit an existing note by ID
     @access Private (Production) | Public (Development)
 */
-router.put("/:id", (req, res) => {
-  const id = req.params.id;
-  const note = req.body;
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const id = req.params.id;
+    const note = req.body;
 
-  Note.findByIdAndUpdate(id, note)
-    .then(() => {
-      res.status(200);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+    Note.findByIdAndUpdate(id, note)
+      .then(() => {
+        res.status(200);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  }
+);
 
 /*
     @route  DELETE notes/:id
     @desc   Delete a note by ID
     @access Private (Production) | Public (Development)
 */
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const id = req.params.id;
 
-  Note.findByIdAndRemove(id)
-    .then(() => {
-      res.status(200);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+    Note.findByIdAndRemove(id)
+      .then(() => {
+        res.status(200);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  }
+);
 
 module.exports = router;
